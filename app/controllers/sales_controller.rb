5bac00f0da4b0bc3701d8eaa043c71b914f87e0a
lukas <lukas.bitter@gmail.com>
2016-03-23 16:09:47 +0100

    - add sales_controller  buy and show methods - add sales index and show views

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -3,8 +3,14 @@ class SalesController < ApplicationController
     @sales = Sale.all
   end
 
-  def create
-    @sale = Sale.create.params[:sale]
-    redirect_to articles
+  def buy
+    @sale = Sale.find(params[:id])
+    @sale.update(price: @sale.price + 1)
+
+    redirect_to sales_url
+  end
+
+  def show
+    @sale = Sale.find(params[:id])
   end
 end

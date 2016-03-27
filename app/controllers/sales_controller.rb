0f937fc92bbd40fd7ce7bdde19a7bdf03a755c93
lukas <lukas.bitter@gmail.com>
2016-03-27 20:40:16 +0200

    add user auhentication before buy action

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,11 +1,14 @@
 class SalesController < ApplicationController
+  before_action :authenticate_user!, only: [:buy]
+
   def index
     @sales = Sale.all
   end
 
   def buy
     @sale = Sale.find(params[:id])
-    @sale.update(price: @sale.price + 1)
+    @user = User.find(params[:user_id])
+    @sale.update(price: @sale.price + 1, user: @user)
 
     redirect_to :back
   end
